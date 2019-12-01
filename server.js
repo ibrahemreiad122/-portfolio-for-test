ff576f58fef36af06fe517946fd1ba3fc167d1e0
ibrahem <ibrahem.reiad@gmail.com>
2019-12-02 00:51:14 +0200

    modify in server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express"),
   app = express(),
   ejs = require("ejs"),
-  path = require("path");
+  path = require("path"),
+  fs = require("fs"),
+  bodyParser = require("body-parser"),
+  urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use((req, res, next) => {
+  var time = new Date().toString();
+  var url = req.url;
+  fs.appendFileSync("text.txt", time + url + "\n");
   next();
 });
 app.get("/", (req, res) => {
@@ -12,6 +18,11 @@ app.get("/", (req, res) => {
 });
 app.get("/portfolio", (req, res) => {
   res.render("portfolio.ejs");
+});
+app.post("/portfolio", urlencodedParser, (req, res) => {
+  res.render("contact.ejs", {
+    welcome: `hello ${req.body.firstName}`
+  });
 });
 app.get("/home", (req, res) => {
   res.render("portfolio.ejs");
